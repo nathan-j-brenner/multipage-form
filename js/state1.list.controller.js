20fd3ca9fcef7b913d35421f6433235d2bce6f18
nathan-j-brenner <nathan.j.brenner@gmail.com>
2016-05-08 10:21:49 -0700

    removed unnecessary data from controllers

--- js/state1.list.controller.js
+++ js/state1.list.controller.js
@@ -2,7 +2,6 @@ angular.module('myApp').controller('State1List', State1List);
 
 function State1List(messages){
 	var vm = this;
-	vm.items = ["A", "List", "Of", "Items"];
 	
 	vm.newMessage = "Hello World!";
 	
--- js/state2.list.controller.js
+++ js/state2.list.controller.js
@@ -2,6 +2,5 @@ angular.module('myApp').controller('State2Items', State2Items);
 
 function State2Items(messages) {
 	var vm = this;
-	vm.things = ["A", "Set", "Of", "Things"];
 	vm.messages = messages.list;
 }
