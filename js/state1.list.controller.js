d582a0f80b02eda54589a93d6c7390af94227705
nathan-j-brenner <nathan.j.brenner@gmail.com>
2016-05-08 10:19:27 -0700

    added message service

--- js/state1.list.controller.js
+++ js/state1.list.controller.js
@@ -1,6 +1,13 @@
 angular.module('myApp').controller('State1List', State1List);
 
-function State1List($scope){
+function State1List(messages){
 	var vm = this;
 	vm.items = ["A", "List", "Of", "Items"];
+	
+	vm.newMessage = "Hello World!";
+	
+	vm.addMessage = function (message) {
+		messages.add(message);
+		vm.newMessage = '';
+	};
 }
--- js/state2.list.controller.js
+++ js/state2.list.controller.js
@@ -1,6 +1,7 @@
 angular.module('myApp').controller('State2Items', State2Items);
 
-function State2Items() {
+function State2Items(messages) {
 	var vm = this;
 	vm.things = ["A", "Set", "Of", "Things"];
+	vm.messages = messages.list;
 }
