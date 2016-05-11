4ede19221432744f9a092f1bb5c9aecf1a74efb0
nathan-j-brenner <nathan.j.brenner@gmail.com>
2016-05-11 01:27:06 -0700

    folders by feature structure

--- app.js
+++ app.js
@@ -7,27 +7,27 @@ $urlRouterProvider.otherwise("/state1");
 $stateProvider
 	.state('state1', {
 		url: "/state1",
-		templateUrl: "partials/form-state1.html",
+		templateUrl: "form-state1/form-state1.html",
 		controller: "State1 as vm"
 	})
 	.state('state2', {
 		url: '/state2',
-		templateUrl: 'partials/form-state2.html',
+		templateUrl: 'form-state2/form-state2.html',
 		controller: 'State2 as vm'
 	})
 	.state('state3', {
 		url: '/state3',
-		templateUrl: 'partials/form-state3.html',
+		templateUrl: 'form-state3/form-state3.html',
 		controller: 'State3 as vm'
 	})
 	.state('review', {
 		url: "/review",
-		templateUrl: "partials/form-review.html",
+		templateUrl: "form-review/form-review.html",
 		controller: 'Review as vm'
 	})
 	.state('submit', {
 		url: '/submit',
-		templateUrl: 'partials/form-submit.html',
+		templateUrl: 'form-submit/form-submit.html',
 		controller: 'Submit as vm'
 	});
 });
